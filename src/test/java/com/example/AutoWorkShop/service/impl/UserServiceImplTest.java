e84176951186fbc0268064e7089cd95424e5a8d8
dddobrev <dddobrev@abv.bg>
2021-04-04 14:40:06 +0300

    some test working

--- src/test/java/com/example/AutoWorkShop/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/AutoWorkShop/service/impl/UserServiceImplTest.java
@@ -5,7 +5,6 @@ import com.example.AutoWorkShop.domain.entities.UserRoleEntity;
 import com.example.AutoWorkShop.domain.entities.enums.UserRoleEnum;
 import com.example.AutoWorkShop.repository.UserRepository;
 import com.example.AutoWorkShop.repository.UserRoleRepository;
-import com.example.AutoWorkShop.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,26 +12,25 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@Transactional
 public class UserServiceImplTest {
-    UserService serviceToTest;
+
+    UserServiceImpl serviceToTest;
     UserEntity mitko;
     UserEntity dido;
     UserRoleEntity admin;
     UserRoleEntity user;
 
     @Mock
-    UserRoleRepository userRoleRepository;
+    UserRoleRepository mockUserRoleRepository;
     @Mock
     UserRepository mockedUserRepository;
     @Mock
@@ -53,20 +51,21 @@ public class UserServiceImplTest {
         user = new UserRoleEntity();
         user.setRole(UserRoleEnum.USER);
         passwordEncoder = new BCryptPasswordEncoder();
-        serviceToTest = new UserServiceImpl(userRoleRepository, mockedUserRepository, passwordEncoder, modelMapper, userSecurity);
+        serviceToTest = new UserServiceImpl(mockUserRoleRepository, mockedUserRepository, passwordEncoder, modelMapper, userSecurity);
         mitko = new UserEntity();
-        mitko.setUsername("mitko").setPassword("123").setEmail("m@m").setUserRoles(List.of(admin));
+        mitko.setUsername("mitko").setPassword(passwordEncoder.encode("123")).setEmail("m@m").setUserRoles(List.of(admin));
         dido = new UserEntity();
-        dido.setUsername("dido").setPassword("321").setEmail("d@d").setUserRoles(List.of(user));
+        dido.setUsername("dido").setPassword(passwordEncoder.encode("321")).setEmail("d@d").setUserRoles(List.of(user));
 
     }
 
-//
-//    @Test
-//    void getAllUsers() {
-//        Mockito.when(mockedUserRepository.findAll()).thenReturn(List.of(mitko, dido));
-//        List<String> allFindUsers = serviceToTest.getAllUsersNames();
-//        assertEquals(allFindUsers.get(0), dido.getUsername());
-//        assertEquals(allFindUsers.get(1), mitko.getUsername());
-//    }
+
+    @Test
+    void getAllUsers() {
+
+        when(serviceToTest.getAllUsersNames()).thenReturn(List.of(mitko.getUsername(), dido.getUsername()));
+        List<String> allFindUsers = serviceToTest.getAllUsersNames();
+        assertEquals(allFindUsers.get(0), mitko.getUsername());
+        assertEquals(allFindUsers.get(1), dido.getUsername());
+    }
 }
---
+++ src/test/java/com/example/AutoWorkShop/web/ClientControllerTest.java
@@ -0,0 +1,4 @@
+package com.example.AutoWorkShop.web;
+
+public class ClientControllerTest {
+}
